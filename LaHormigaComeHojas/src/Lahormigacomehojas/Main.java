068401955cb00096bba2e0ae9e6ce98db22f4160
Zygmut <Zygmut@users.noreply.github.com>
2019-05-18 15:18:55 +0200

    Ver 01
    -Codigo comentado

--- LaHormigaComeHojas/src/Lahormigacomehojas/Main.java
+++ LaHormigaComeHojas/src/Lahormigacomehojas/Main.java
@@ -32,7 +32,7 @@ public class Main extends JFrame implements KeyListener {
             @Override
             public void run() {
                 Main frame = new Main();
-                frame.setTitle("Juego de la hormiga de mierda esta que me tiene hasta la polla");
+                frame.setTitle("Juego de la hormiga come hojas");
                 frame.setResizable(false);
                 frame.setSize(frame.board.getPreferredSize());
                 frame.setLocationRelativeTo(null);
@@ -47,7 +47,14 @@ public class Main extends JFrame implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         int xpos = 0, ypos = 0;
-        if (e.getKeyCode() == 32) { //space
+        /**
+         * Controlador del espacio
+         */
+        if (e.getKeyCode() == 32) { 
+            /**
+             * Algoritmo para conocer la posicion de la hormiga dentro de la
+             * matriz
+             */
             boolean found = false;
             for (int i = 0; i < Board.Dim && !found; i++) {
                 for (int j = 0; j < Board.Dim && !found; j++) {
@@ -58,11 +65,22 @@ public class Main extends JFrame implements KeyListener {
                     }
                 }
             }
-            String orientation = board.cBoard[xpos][ypos].piece.getId().split("_")[1]; //orientacion de la hormiga
+            /**
+             *
+             */
+            String orientation = board.cBoard[xpos][ypos].piece.getId().split("_")[1];
+            /**
+             * Switch que controla el movimiento de la hormiga, dejando piezas
+             * vacias donde estaba y moviendola dependiendo de su orientacion
+             */
             switch (orientation) {
                 case ("n"):
                     board.cBoard[xpos][ypos].setPiece(new Piece(Piece.nada));
                     xpos -= 1;
+                    /**
+                     * Control del teletransporte, se aplica a todos los otros
+                     * casos teniendo en cuenta sus variaciones
+                     */
                     if (xpos < 0) {
                         xpos = Board.Dim - 1;
                     }
@@ -95,58 +113,70 @@ public class Main extends JFrame implements KeyListener {
             }
             board.repaint();
 
-        } else if (e.getKeyCode() == 37 || e.getKeyCode() == 38 || e.getKeyCode() == 39 || e.getKeyCode() == 40) { //Arrow keys
+        } else {
+            /**
+             * Controlador de las flechas
+             */
+            if (e.getKeyCode() == 37 || e.getKeyCode() == 38 || e.getKeyCode() == 39 || e.getKeyCode() == 40) {
+                /**
+                 * Algoritmo de busqueda de la hormiga
+                 */
+                boolean found = false;
+                for (int i = 0; i < Board.Dim && !found; i++) {
+                    for (int j = 0; j < Board.Dim && !found; j++) {
+                        found = board.cBoard[i][j].piece.getId().contains("hormiga");
+                        if (found) {
+                            xpos = i;
+                            ypos = j;
+                        }
+                    }
+                }
+                /**
+                 * Switch que permite cambiar la orientacion de la hormiga
+                 */
+                switch (e.getKeyCode()) {
+                    case (39):
+                        board.cBoard[xpos][ypos].setPiece(new Piece(Piece.hormigaE));
+                        break;
+                    case (38):
+                        board.cBoard[xpos][ypos].setPiece(new Piece(Piece.hormigaN));
+                        break;
+                    case (37):
+                        board.cBoard[xpos][ypos].setPiece(new Piece(Piece.hormigaO));
+                        break;
+                    case (40):
+                        board.cBoard[xpos][ypos].setPiece(new Piece(Piece.hormigaS));
+                        break;
+                }
+                board.repaint();
+            }
+            /**
+             * Win condition
+             */
             boolean found = false;
             for (int i = 0; i < Board.Dim && !found; i++) {
                 for (int j = 0; j < Board.Dim && !found; j++) {
-                    found = board.cBoard[i][j].piece.getId().contains("hormiga");
-                    if (found) {
-                        xpos = i;
-                        ypos = j;
-                    }
+                    found = board.cBoard[i][j].piece.getId().contains("hoja");
                 }
             }
-            switch (e.getKeyCode()) {
-                case (39):
-                    board.cBoard[xpos][ypos].setPiece(new Piece(Piece.hormigaE));
-                    break;
-                case (38):
-                    board.cBoard[xpos][ypos].setPiece(new Piece(Piece.hormigaN));
-                    break;
-                case (37):
-                    board.cBoard[xpos][ypos].setPiece(new Piece(Piece.hormigaO));
-                    break;
-                case (40):
-                    board.cBoard[xpos][ypos].setPiece(new Piece(Piece.hormigaS));
-                    break;
-            }
-            board.repaint();
-        }
-        boolean found = false;
-        for (int i = 0; i < Board.Dim && !found; i++) {
-            for (int j = 0; j < Board.Dim && !found; j++) {
-                found = board.cBoard[i][j].piece.getId().contains("hoja");
-            }
-        }
-        if (!found) {
-            Toolkit.getDefaultToolkit().beep();
-            JOptionPane.showMessageDialog(null, "ENHORABUENA" + "\n"
-                    + "¡Has completado el juego!" + "\n"
-                    + "La hormiga se ha comido todas las hojas",
-                    "¡Has ganado!", JOptionPane.INFORMATION_MESSAGE);
-            if (JOptionPane.INFORMATION_MESSAGE == 1) {
-                System.exit(0);
+            if (!found) {
+                Toolkit.getDefaultToolkit().beep();
+                JOptionPane.showMessageDialog(null, "ENHORABUENA" + "\n"
+                        + "¡Has completado el juego!" + "\n"
+                        + "La hormiga se ha comido todas las hojas",
+                        "¡Has ganado!", JOptionPane.INFORMATION_MESSAGE);
+                if (JOptionPane.INFORMATION_MESSAGE == 1) {
+                    System.exit(0);
+                }
             }
         }
     }
 
     @Override
-    public void keyTyped(KeyEvent ke
-    ) {
+    public void keyTyped(KeyEvent ke) {
     }
 
     @Override
-    public void keyReleased(KeyEvent ke
-    ) {
+    public void keyReleased(KeyEvent ke) {
     }
 }
